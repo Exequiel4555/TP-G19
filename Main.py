6049e2c3ec92111856dac3db5443bbbd4e472228
Exequiel4555 <evillaparedes@uade.edu.ar>
2024-08-26 20:08:50 -0300

    V0.1

---
+++ Main.py
@@ -0,0 +1,21 @@
+from termcolor import colored
+
+def Nueva_Partida():
+    pass
+def Instrucciones():
+    pass
+
+def main():
+    try:
+        print(colored(f"\nBIENVENIDO A SUDOKU\n","red"))
+        modo = input(f"Seleccionar Modo \n1)Nueva Partida\n2)Instrucciones\n")
+        
+    except:
+        pass
+
+
+    
+
+
+if __name__=="__main__":
+    main()
