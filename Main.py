f6ecdb17de7eaa6318826d7abfa54bcb5c8cb571
Exequiel4555 <evillaparedes@uade.edu.ar>
2024-11-16 05:31:37 -0300

    Main.py

--- Main.py
+++ Main.py
@@ -1,55 +1,39 @@
-from termcolor import colored
-from random import randint,shuffle
-from pyfiglet import Figlet
-import os
-import re
+from termcolor import colored  
+from random import randint, shuffle  
+from pyfiglet import Figlet  
+import os  
+import time
 
-# ------------------------------------------------------- BANNER
-def Banner():
-    """Muestra el Banner principal.
-    Args:
-        None.
-    Returns:
-        Retorna el banner principal.
+# ------------------------------------------------------- BANNER  
+def Banner():  
+    """Muestra el Banner principal.  
+    Args:  
+        None.  
+    Returns:  
+        Retorna el banner principal.  
     """  
     f = Figlet(font="smslant")  
-    print(colored(f.renderText("SUDOKU"), "blue"))
-#---------------------------------------------------------USUARIO
-def validar_usuario():  
+    print(colored(f.renderText("SUDOKU"), "blue"))  
+
+# ------------------------------------------------------- MATRIZ  
+def Tablero():  
+    """Genera tablero.  
+    Args:  
+        None.  
+    Returns:  
+        Retorna el tablero 9x9.  
     """  
-    Valida si el nombre de usuario cumple con los siguientes criterios:  
-    - Entre 3 y 15 caracteres.  
-    - Solo contiene letras mayúsculas y minúsculas, y números.  
-    """
-    usuario = input("Ingrese su nombre de usuario: ")  
-    patron_usuario = r'^[a-zA-Z0-9]{3,15}$'  
-    if not re.match(patron_usuario, usuario): 
-        print(colored("[!] El nombre de usuario no es válido. Debe tener entre 3 y 15 caracteres y solo incluir letras y números.","red"))
-        usuario = input("Ingrese su nombre de usuario: ")   
-    return usuario 
+    tablero = [[0] * 9 for _ in range(9)]  
+    Resolver(tablero)  
+    Cambiar_valores(tablero)  
+    return tablero  
 
-
-# ------------------------------------------------------- MATRIZ
-def Tablero():
-    """Genera tablero.
-    Args:
-        None.
-    Returns:
-        Retorna el tablero 9x9.
-
-    """
-    tablero = [[0] * 9 for _ in range(9)]
-    Resolver(tablero)
-    Cambiar_valores(tablero)
-    return tablero
-    
-
-def Mostrar_tablero(matriz):
-    """Muestra el tablero en pantalla.
-    Args:
-        Tablero.
-    Returns:
-       None.
+def Mostrar_tablero(matriz):  
+    """Muestra el tablero en pantalla.  
+    Args:  
+        Tablero.  
+    Returns:  
+       None.  
     """  
     print("\n")  
     for f in range(9):  
@@ -66,64 +50,62 @@ def Mostrar_tablero(matriz):
         print()  
     print(" ")  
 
-def Resolver(tablero):
-    """Resuelve el tablero.
-    Args:
-        Tablero.
-    Returns:
-       None.
+def Resolver(tablero):  
+    """Resuelve el tablero.  
+    Args:  
+        Tablero.  
+    Returns:  
+       None.  
     """  
-    for fila in range(9):
-        for columna in range(9):
-            if tablero[fila][columna] == 0:
-                numeros = list(range(1, 10))
-                shuffle(numeros) 
-                for num in numeros:
-                    if es_valido(tablero, fila, columna, num):
-                        tablero[fila][columna] = num
-                        if Resolver(tablero):
-                            return True
-                        tablero[fila][columna] = 0
-                return False
-    return True
+    for fila in range(9):  
+        for columna in range(9):  
+            if tablero[fila][columna] == 0:  
+                numeros = list(range(1, 10))  
+                shuffle(numeros)   
+                for num in numeros:  
+                    if es_valido(tablero, fila, columna, num):  
+                        tablero[fila][columna] = num  
+                        if Resolver(tablero):  
+                            return True  
+                        tablero[fila][columna] = 0  
+                return False  
+    return True  
 
-
-def es_valido(tablero, fila, columna, num):
-    """Verifica el tablero si esta correctamente.
-    Args:
-        Tablero.
-        Fila.
-        Columna.
-        Num(es el numero de la lista numeros).
-    Returns:
-       True(es valido por que verifica si no hay ningun numero repetido en la fila,columna y subcuadro 3x3).
+def es_valido(tablero, fila, columna, num):  
+    """Verifica el tablero si esta correctamente.  
+    Args:  
+        Tablero.  
+        Fila.  
+        Columna.  
+        Num(es el numero de la lista numeros).  
+    Returns:  
+       True(es valido por que verifica si no hay ningun numero repetido en la fila,columna y subcuadro 3x3).  
     """  
-    # Verificar fila
-    if num in tablero[fila]:
-        return False
+    # Verificar fila  
+    if num in tablero[fila]:  
+        return False  
 
-    # Verificar columna
-    for i in range(9):
-        if tablero[i][columna] == num:
-            return False
+    # Verificar columna  
+    for i in range(9):  
+        if tablero[i][columna] == num:  
+            return False  
 
-    # Verificar subcuadro 3x3
-    inicio_fila, inicio_columna = 3 * (fila // 3), 3 * (columna // 3)
-    for i in range(inicio_fila, inicio_fila + 3):
-        for j in range(inicio_columna, inicio_columna + 3):
-            if tablero[i][j] == num:
-                return False
+    # Verificar subcuadro 3x3  
+    inicio_fila, inicio_columna = 3 * (fila // 3), 3 * (columna // 3)  
+    for i in range(inicio_fila, inicio_fila + 3):  
+        for j in range(inicio_columna, inicio_columna + 3):  
+            if tablero[i][j] == num:  
+                return False  
 
-    return True
-
+    return True  
 
 def Cambiar_valores(tablero, vaciar=2):  
-    """Remueve números del tablero para crear un rompecabezas con la cantidad deseada de celdas vacías.
-    Args:
-        Tablero.
-        Vaciar(genera un espacio).
-    Returns:
-       Tablero.
+    """Remueve números del tablero para crear un rompecabezas con la cantidad deseada de celdas vacías.  
+    Args:  
+        Tablero.  
+        Vaciar(genera un espacio).  
+    Returns:  
+       Tablero.  
     """  
     count = 0  
     while count < vaciar:  
@@ -132,30 +114,26 @@ def Cambiar_valores(tablero, vaciar=2):
         if tablero[fila][col] != 0:  
             tablero[fila][col] = 0  
             count += 1  
-    return tablero 
+    return tablero   
     
 def tablero_completo(tablero):  
-    """Verifica si el tablero está completo.
-    Args:
-        Tablero.
-        Vaciar(genera un espacio o input).
-    Returns:
-       Tablero.
+    """Verifica si el tablero está completo.  
+    Args:  
+        Tablero.  
+        Vaciar(genera un espacio o input).  
+    Returns:  
+       Tablero.  
     """  
     for fila in tablero:  
         if 0 in fila:  
-            return False
-    return True
-
+            return False  
+    return True  
 
 # -------------------------------------------------------------- INPUTS Y VALIDACIONES  
 def posicion_num(tablero):   
     while True:    
-        fila = int(input("[+] Ingresar Fila (1-9) : "))-1  
-        if fila == -2:  
-            return -1, -1  # Código especial para deshacer  
-        
-        columna = int(input("[+] Ingresar Columna (1-9): "))-1  
+        fila = int(input("[+] Ingresar Fila (1-9) : ")) - 1  
+        columna = int(input("[+] Ingresar Columna (1-9): ")) - 1  
 
         if 0 <= fila < 9 and 0 <= columna < 9:  
             if tablero[fila][columna] == 0:  
@@ -165,30 +143,28 @@ def posicion_num(tablero):
         else:  
             print(colored("[!] Fila o columna fuera de rango. Deben ser entre 1 y 9.", "red"))  
 
-
 def insert_num(tablero, num, fila, columna):  
-    """Intenta insertar un número en el tablero en la posición dada.
-    Args:
-        Tablero.
-        Num.
-        Fila.
-        Columna.
-    Returns:
-       True(Si el numero ingresado es correcto).
-       False(Si el numero ingresado es incorrecto).
+    """Intenta insertar un número en el tablero en la posición dada.  
+    Args:  
+        Tablero.  
+        Num.  
+        Fila.  
+        Columna.  
+    Returns:  
+       True(Si el numero ingresado es correcto).  
+       False(Si el numero ingresado es incorrecto).  
     """  
     if es_valido(tablero, fila, columna, num):  
         tablero[fila][columna] = num  
         return True  
     return False  
 
-
 def Seleccion_numero():  
-    """Solicita al usuario un número entre 1 y 9.
-    Args:
-        None
-    Returns:
-       True(Si el numero ingresado es correcto).
+    """Solicita al usuario un número entre 1 y 9.  
+    Args:  
+        None  
+    Returns:  
+       True(Si el numero ingresado es correcto).  
     """  
     while True:  
         try:  
@@ -199,96 +175,81 @@ def Seleccion_numero():
                 print(colored("[!] Número inválido. Debe ser entre 1 y 9.", "red"))  
         except ValueError:  
             print(colored("[!] Entrada inválida. Por favor, ingresa un número entre 1 y 9.", "red"))  
-#-----------------------------------------------------------MOVIMINETO 
-def deshacer_movimiento(historial, tablero):  
-    """Deshace el último movimiento realizado por el usuario."""  
-    if historial:  
-        ult_fila, ult_col, _ = historial.pop()  
-        tablero[ult_fila][ult_col] = 0  
-        print(colored("[+] Movimiento deshecho con éxito.", "green"))  
-    else:  
-        print(colored("[!] No hay movimientos para deshacer.", "yellow"))
 
-#-----------------------------------------------------------DIFICULTAD_NORMAL
-def Dificultad_normal(tablero,usuario):
-    """PARTIDA DNORMAL.
-    Args:
-       Tablero
-    Returns:
-       None
-    """  
-    error = 0
-    historial = []
-    while not tablero_completo(tablero):
-        Mostrar_tablero(tablero)
-        numero = Seleccion_numero()
-        fila, columna = posicion_num(tablero)
-        if fila == -1 and columna == -1:  
-            deshacer_movimiento(historial, tablero)  
-            continue
-        if not insert_num(tablero, numero,fila, columna):
-            error+=1
-            print(colored(f"[!] ERROR: Número {numero} no válido en la posición ({fila +1}, {columna +1}).", "red"))
-            if error >= 3:
-                print(colored("[!] Has alcanzado el límite de 3 errores. Juego terminado.", "red"))
+#-----------------------------------------------------------DIFICULTAD_NORMAL  
+def Dificultad_normal(tablero):  
+    """PARTIDA NORMAL.  
+    Args:  
+       Tablero  
+    Returns:  
+       None  
+    """ 
+    start_time = time.time() 
+    error = 0  
+    while not tablero_completo(tablero):  
+        Mostrar_tablero(tablero)  
+        numero = Seleccion_numero()  
+        fila, columna = posicion_num(tablero)  
+        
+        if not insert_num(tablero, numero, fila, columna):  
+            error += 1  
+            print(colored(f"[!] ERROR: Número {numero} no válido en la posición ({fila + 1}, {columna + 1}).", "red"))  
+            if error >= 3:  
+                print(colored("[!] Has alcanzado el límite de 3 errores. Juego terminado.", "red"))  
                 main()
-        else:  
-            # Guardar el movimiento en el historial  
-            historial.append((fila, columna, numero))
-        Mostrar_tablero(tablero)
-
-
-
-#--------------------------------------------------------------DIFICULTAD_EXTREMA
-def Dificultad_extremo(tablero):
-    """PARTIDA DIFICULTAD EXTREMA.
-    Args:
-       Tablero
-    Returns:
-       None
+    end_time = time.time()  
+    total_time = end_time - start_time   
+    Mostrar_tablero(tablero)  
+    print(colored("[+] ¡Felicidades! Has completado el Sudoku correctamente.", "green"))  
+    print(colored(f"Tiempo total jugado: {total_time:.2f} segundos", "yellow"))  
+#--------------------------------------------------------------DIFICULTAD_EXTREMA  
+def Dificultad_extremo(tablero):  
+    """PARTIDA DIFICULTAD EXTREMA.  
+    Args:  
+       Tablero  
+    Returns:  
+       None  
     """  
-    try: 
-        while True:
-            Mostrar_tablero(tablero)
-            numero = Seleccion_numero()
-            Fila, Columna = posicion_num(tablero)
-            if not insert_num(tablero, numero, Fila, Columna):
-                lista = listar_archivos() 
-                borrar_archivos(lista)
-            Mostrar_tablero(tablero)
-            tablero_completo(tablero)
-            numero = Seleccion_numero()
-    except (IndexError,ValueError,TypeError):
-        print(colored("[!] Indice fuera de rango","red"))
+    try:   
+        while True:  
+            Mostrar_tablero(tablero)  
+            numero = Seleccion_numero()  
+            Fila, Columna = posicion_num(tablero)  
+            if not insert_num(tablero, numero, Fila, Columna):  
+                lista = listar_archivos()   
+                borrar_archivos(lista)  
+            Mostrar_tablero(tablero)  
+            tablero_completo(tablero)  
+            numero = Seleccion_numero()  
+    except (IndexError, ValueError, TypeError):  
+        print(colored("[!] Indice fuera de rango", "red"))  
 
+listar_archivos = lambda: list(filter(os.path.isfile, os.listdir('.')))   
 
-listar_archivos = lambda: list(filter(os.path.isfile, os.listdir('.'))) 
-
-def borrar_archivos(lista):
-    """Borrar el primer archivo de la lista proporcionda.
-    Args:
-       Lista
-    Returns:
-       None
+def borrar_archivos(lista):  
+    """Borrar el primer archivo de la lista proporcionda.  
+    Args:  
+       Lista  
+    Returns:  
+       None  
     """  
-    if not lista:
-        print(colored("[!] No hay archivos para borrar.", "yellow"))
-        return
-    nombre_archivo = lista[0]
+    if not lista:  
+        print(colored("[!] No hay archivos para borrar.", "yellow"))  
+        return  
+    nombre_archivo = lista[0]  
     try:         
         os.remove(nombre_archivo)  
-        print(colored(f"[!] El archivo '{nombre_archivo}' ha sido borrado.","red"))
-    except OSError as e:
+        print(colored(f"[!] El archivo '{nombre_archivo}' ha sido borrado.", "red"))  
+    except OSError as e:  
         print(colored(f"[!] Error al borrar el archivo '{nombre_archivo}': {e}", "red"))  
-    
 
-#---------------------------------------------------------- MENÚ DE OPCIONES
+#---------------------------------------------------------- MENÚ DE OPCIONES  
 def Mostrar_inicio():  
-    """Muestra las opciones iniciales del juego y retorna la elección del usuario.
-    Args:
-       None
-    Returns:
-       Choice(las opciones a elegir del menu)
+    """Muestra las opciones iniciales del juego y retorna la elección del usuario.  
+    Args:  
+       None  
+    Returns:  
+       Choice(las opciones a elegir del menu)  
     """  
     opciones = {  
         1: "Nueva Partida",  
@@ -308,20 +269,19 @@ def Mostrar_inicio():
         except ValueError:  
             print(colored("[!] Entrada inválida. Por favor, ingresa un número correspondiente a las opciones.", "red"))  
 
-
 def Mostrar_Modo():  
-    """Muestra los modos de dificultad y retorna la elección del usuario.
-    Args:
-       None
-    Returns:
-       Modo(las opciones a elegir del modo de juego)
-    """ 
+    """Muestra los modos de dificultad y retorna la elección del usuario.  
+    Args:  
+       None  
+    Returns:  
+       Modo(las opciones a elegir del modo de juego)  
+    """   
     modos = {  
         1: "Normal",  
         2: "Extremo",  
         3: "Regresar al Menú Principal"  
     }  
-    print(colored("\n===== SELECCIONA DIFICULTAD =====","magenta"))  
+    print(colored("\n===== SELECCIONA DIFICULTAD =====", "magenta"))  
     for clave, valor in modos.items():  
         print(f"{clave}. {valor}")  
     while True:  
@@ -332,42 +292,40 @@ def Mostrar_Modo():
             else:  
                 print(colored("[!] Modo inválido. Selecciona una opción válida.", "red"))  
         except ValueError:  
-            print(colored("[!] Entrada inválida. Por favor, ingresa un número correspondiente a las opciones.", "red"))
+            print(colored("[!] Entrada inválida. Por favor, ingresa un número correspondiente a las opciones.", "red"))  
 
-#---------------------------------------------------------------- INTRUCCIONES
-def Mostrar_Instrucciones():
-    """Muesta las instruccion del juego. 
-    Args:
-       None
-    Returns:
-       Instrucciones
-    """ 
-    instrucciones = """
-El objetivo del Sudoku es llenar todas las celdas vacías en un tablero de orden 9 de manera que:
+#---------------------------------------------------------------- INTRUCCIONES  
+def Mostrar_Instrucciones():  
+    """Muesta las instruccion del juego.   
+    Args:  
+       None  
+    Returns:  
+       Instrucciones  
+    """   
+    instrucciones = """  
+El objetivo del Sudoku es llenar todas las celdas vacías en un tablero de orden 9 de manera que:  
 
-- Cada fila contenga todos los numeros del 1 al 9 sin repetir ninguno.
-- Cada columna contenga todos los números del 1 al 9 sin repetir ninguno.
-- Cada una de las nueve subcuadriculas de 3x3 contengan todos los números del 1 al 9 sin repetir ninguno.
+- Cada fila contenga todos los numeros del 1 al 9 sin repetir ninguno.  
+- Cada columna contenga todos los números del 1 al 9 sin repetir ninguno.  
+- Cada una de las nueve subcuadriculas de 3x3 contengan todos los números del 1 al 9 sin repetir ninguno.  
 
-¡Buena suerte! 
-"""
+¡Buena suerte!   
+"""  
     print(colored(instrucciones, "green"))  
 
-
-# ----------------------------------------------------------------INICIO DEL JUEGO
+# ----------------------------------------------------------------INICIO DEL JUEGO  
 def Inicio():  
-    """Gestiona la elección inicial del usuario """
+    """Gestiona la elección inicial del usuario """  
     try:   
-        Continuar = True
-        while Continuar:
-            usuario = validar_usuario()  
+        Continuar = True  
+        while Continuar:  
             choice = Mostrar_inicio()  
             if choice == 1:  
                 while True:  
                     modo = Mostrar_Modo()  
                     if modo == 1:  
                         tablero = Tablero()  
-                        Dificultad_normal(tablero,usuario)  
+                        Dificultad_normal(tablero)  
                     elif modo == 2:  
                         tablero = Tablero()   
                         Dificultad_extremo(tablero)  
@@ -376,16 +334,16 @@ def Inicio():
             elif choice == 2:  
                 Mostrar_Instrucciones()  
             elif choice == 3:  
-                Continuar = False
-    except KeyboardInterrupt as e:
-        print(e)
-    finally:
-        print(colored("\n[+] ¡Gracias por jugar Sudoku! Hasta la próxima.", "cyan"))
+                Continuar = False  
+    except KeyboardInterrupt as e:  
+        print(e)  
+    finally:  
+        print(colored("\n[+] ¡Gracias por jugar Sudoku! Hasta la próxima.", "cyan"))  
 
-# ------------------------------------------------------- PROGRAMA PRINCIPAL
-def main():
-    Banner()
-    Inicio()
+# ------------------------------------------------------- PROGRAMA PRINCIPAL  
+def main():  
+    Banner()  
+    Inicio()  
 
-if __name__ == "__main__":
+if __name__ == "__main__":  
     main()
